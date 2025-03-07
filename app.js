5abe19c591b3c0f50f7d6acc259940ab27769c12
julesdmai <julesdmai@gmail.com>
2025-03-06 20:54:10 -0800

    Replacing innerHTML

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let myNav = document.querySelector("nav");
+    console.log({myNav});
+    myNav.innerHTML = `
+        <h2>hello world</h2>
+        <p>hello jules</p>
+    `
+});
